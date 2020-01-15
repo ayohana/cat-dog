6085a368c96f753a7ea8f188afd5d78c88ccbc12
Adela <adela.yohana@gmail.com>
2020-01-14 18:26:33 -0800

    Add responses on clicking action to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+  $("button#cat").click(function() {
+    $("ul#cat-says").append("<li>MEOW!</li>");
+    $("ul#dog-says").append("<li>Can you downward dawg?</li>");
+  });
+  $("button#dog").click(function() {
+    $("ul#dog-says").append("<li>WOOF!</li>");
+    $("ul#cat-says").append("<li>Marjaryasana...Do the cat pose!</li>");
+  });
 
 
-  
 });
