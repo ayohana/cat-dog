678ed573ef113759bc7a31d62beb3e09fae0a580
Adela <adela.yohana@gmail.com>
2020-01-14 18:30:14 -0800

    Remove previous response on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
+    $("li").remove();
     $("ul#cat-says").append("<li>MEOW!</li>");
     $("ul#dog-says").append("<li>Can you downward dawg?</li>");
   });
   $("button#dog").click(function() {
+    $("li").remove();
     $("ul#dog-says").append("<li>WOOF!</li>");
     $("ul#cat-says").append("<li>Marjaryasana...Do the cat pose!</li>");
   });
